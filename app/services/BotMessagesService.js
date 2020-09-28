2fe192acfb1a9d5b2f6966da19cde2665f0f2dde
Thiago Andrade Silva <thiago.adsix@gmail.com>
2020-09-27 23:53:20 -0300

    :construction: Trying to implemment logic to create connections

--- app/services/BotMessagesService.js
+++ app/services/BotMessagesService.js
@@ -7,7 +7,7 @@ module.exports = class BotMessagesService {
     const browser = await puppeteer.launch({
       headless: false,
       args: ['--disable-features=site-per-process'],
-      defaultViewport: { width: 2048, height: 1080 },
+      defaultViewport: { width: 2048, height: 1080, isLandscape: true,  },
     })
     const page = await browser.newPage()
 
@@ -39,6 +39,54 @@ module.exports = class BotMessagesService {
 
     await (await this._nthEmberElement({ n: 5, page })).click()
     await this._screenshot({ page, fileName: 'apply' })
+
+    await page.waitFor(2000)
+    await page.mouse.click(1165, 350, {button: "left",})
+    await page.waitFor(2000)
+    await page.mouse.click(1165, 470, {button: "left"})
+    await page.waitFor(2000)
+    await page.mouse.click(1165, 600, {button: "left"})
+    await page.waitFor(2000)
+    await page.mouse.click(1165, 715, {button: "left"})
+    await page.waitFor(2000)
+    await page.mouse.click(1165, 840, {button: "left"})
+    await page.waitFor(2000)
+    await page.mouse.click(1165, 960, {button: "left"})
+    await page.waitFor(2000)
+    await page.mouse.click(1165, 1090, {button: "left"})
+    await page.waitFor(2000)
+    await page.mouse.click(1165, 1210, {button: "left"})
+    await page.waitFor(2000)
+    await page.mouse.click(1165, 1330, {button: "left"})
+    await page.waitFor(2000)
+    await page.mouse.click(1165, 1460, {button: "left"})
+    await this._screenshot({ page, fileName: 'Test' })
+
+    await page.waitFor(5000)
+
+    // const escapeXpathString = str => {
+    //   const splitedQuotes = str.replace(/'/g, `', "'", '`)
+    //   return `concat('${splitedQuotes}', '')`
+    // }
+
+    // const clickByText = async (page, text) => {
+    //   const escapedText = escapeXpathString(text)
+    //   const linkHandlers = await page.$x(
+    //     `//button[contains(text(), ${escapedText})]`
+    //   )
+
+    //   if (linkHandlers.length > 0) {
+    //     await linkHandlers[0].click()
+    //   } else {
+    //     throw new Error(`Link not found: ${text}`)
+    //   }
+    // }
+    // button[(data - control - name = 'people_profile_card_connect_button')]
+    // await clickByText(page, 'Conectar')
+    // await page.waitForNavigation({ waitUntil: 'load' })
+    // await (await page.$("[data-control-name='srp_profile_actions']")).click({
+    //   button: 'left',
+    // })
 
     await browser.close()
   }
